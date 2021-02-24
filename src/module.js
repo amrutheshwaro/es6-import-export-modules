a7377bd5dd39db2b5e7ccc00db9451a6df48b87c
Amrutheshwar O <55506973+amrutheshwaro@users.noreply.github.com>
2021-02-24 18:16:00 +0530

    final commit

---
+++ src/module.js
@@ -0,0 +1,12 @@
+const pi = 3.1415962;
+
+var doublePi = function () {
+  return 2 * pi;
+};
+
+var triplePi = function () {
+  return 3 * pi;
+};
+
+export default pi;
+export { doublePi, triplePi };
